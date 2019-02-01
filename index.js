3e5e0c76b79e2afb37bc1b2d337cf28a3146d9fb
Jason Velez <jvelez20@yahoo.com>
2019-02-01 15:55:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function moveDodgerright() {
   var left = parseInt(rightNumbers, 10)
  
   if (right > 0) {
-    dodger.style.right = ${right - 1}px
+    dodger.style.right = '${right - 1}px'
   }
 }
